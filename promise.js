e1735bcc44806d5a4e055eb03e24260a25f056a1
paulsoh <yongdoree@gmail.com>
2016-08-15 18:02:08 +0900

    Add basic low level transitions(fulfill, reject)

--- promise.js
+++ promise.js
@@ -7,4 +7,14 @@ function Promise () {
   var state = PENDING;
   var value = null;
   var handlers = [];
+
+  function fulfill(result) {
+    state = FULFILLED;
+    value = result;
+  }
+
+  function reject(error) {
+    state = REJECTED;
+    value = error;
+  }
 }
